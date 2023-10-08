9dad1901a5cf7460b7a324b226fa7e08ed07283d
Muhammad Ananta <91576986+anantaryaaa@users.noreply.github.com>
2023-10-09 03:02:55 +0700

    Add files via upload

---
+++ Pertemuan 6/P6_Dp_FibonacciWRecursion.py
@@ -0,0 +1,15 @@
+def Rec_Fib(n):
+    if n <= 1:
+        return n
+    else:
+        return (Rec_Fib(n-1) + Rec_Fib(n-2))
+
+nterms = 20
+
+# check if nterms value is valid
+if nterms <= 0:
+    print("Please enter a positive integer")
+else:
+    print("Fibonacci sequence: ")
+    for i in range(nterms):
+        print(Rec_Fib(i))
